b3f15389a1d47873eb542df31e2b84b39f74b7f7
Lukas <lszabocs@atsistemas.com>
2018-09-25 10:19:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import Link from './src/components/link';
+import Button from './src/components/button';
+
+export { Link, Button };
---
+++ src/components/link/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Link = ({ href, children }) => ( <a href={ href }>{ children }</a> )
+
+export default Link;
